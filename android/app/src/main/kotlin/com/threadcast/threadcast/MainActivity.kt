c5e9aca92cc37d44050b5388301d959efd20a592
A-Baji <bidabaji@gmail.com>
2026-03-17 20:53:50 -0500

    register LlmPlugin

--- android/app/src/main/kotlin/com/threadcast/threadcast/MainActivity.kt
+++ android/app/src/main/kotlin/com/threadcast/threadcast/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.threadcast.threadcast
 
+import com.threadcast.app.llm.LlmPlugin
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        flutterEngine.plugins.add(LlmPlugin())
+    }
+}
