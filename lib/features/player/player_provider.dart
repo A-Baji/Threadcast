b11aea9e96bf65275c4f3a711899f580ea56069f
A-Baji <bidabaji@gmail.com>
2026-03-23 12:21:19 -0500

    add audio player

--- lib/features/player/player_provider.dart
+++ lib/features/player/player_provider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
+
 import '../../models/episode.dart'; // AppDatabase passed in as parameter -- no provider import needed
 
 final playerProvider = StateNotifierProvider<PlayerNotifier, PlayerState>(
@@ -20,11 +22,19 @@ class PlayerNotifier extends StateNotifier<PlayerState> {
     } catch (_) {}
   }
 
-  Future<void> play() => _player.play();
-  Future<void> pause() => _player.pause();
+  Future<void> play() async {
+    await WakelockPlus.enable(); // keep screen on during playback
+    return _player.play();
+  }
+
+  Future<void> pause() async {
+    await WakelockPlus.disable();
+    return _player.pause();
+  }
+
   Future<void> seekTo(Duration p) => _player.seek(p);
-  Future<void> skipForward() => _player.seek(_player.position + const Duration(seconds: 15));
-  Future<void> skipBackward() => _player.seek(_player.position - const Duration(seconds: 15));
+  Future<void> skipForward() => _player.seek(_player.position + const Duration(seconds: 10));
+  Future<void> skipBackward() => _player.seek(_player.position - const Duration(seconds: 10));
 
   Stream<Duration> get positionStream => _player.positionStream;
   Duration get position => _player.position;
@@ -34,6 +44,12 @@ class PlayerNotifier extends StateNotifier<PlayerState> {
     _player.dispose();
     super.dispose();
   }
+
+  // Expose whether audio is currently playing as a stream
+  Stream<bool> get playingStream => _player.playingStream;
+
+// Change playback speed (0.75, 1.0, 1.25, 1.5)
+  Future<void> setSpeed(double speed) => _player.setSpeed(speed);
 }
 
 class PlayerState {
